5fba1758730f9b2397d597cc5ef4ded775a8ae24
Ajay-AJ7899 <ajayarun7899@gmail.com>
2026-01-23 20:49:19 +0530

    connected backend

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,16 +1,16 @@
-import express from 'express';
+import express from "express";
+
+
 const router = express.Router();
 
-router.get('/signup', (req, res) => { //prefix will be /api/auth/singup
-    res.send('Hello World');
-});
-router.get('/signin', (req, res) => {
-    res.send('Hello World');
-});
-router.get('/logout', (req, res) => {
-    res.send('Hello World');
-});
 
 
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,34 @@
-//also called app.js
-// const express = require('express');
-import express from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
-
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
+import express from "express";
+import cookieParser from "cookie-parser";
 
 
-dotenv.config();
-const app = express();
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
+
+
 const __dirname = path.resolve();
 
-const port = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
+app.use(express.json({ limit: "5mb" })); // req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
 
-//make deployment
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get('*', (_, res) => res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html')));
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// make ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
 
-//router is in rotuer page here it is linked using import and then app.use 
+server.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+  connectDB();
+});
